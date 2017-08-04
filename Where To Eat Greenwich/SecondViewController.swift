58cc2d2ebb46bee5698744673d825dc195ba7a93
Alexander Kerendian <alexanderkerendian@Alexanders-iMac.local>
2017-08-04 17:49:19 -0400

    Initial Commit

---
+++ Where To Eat Greenwich/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Where To Eat Greenwich
+//
+//  Created by Alexander Kerendian on 8/4/17.
+//  Copyright © 2017 Aktrapp. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
